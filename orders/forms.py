5145988c6a5f9567bb3d57ab5cc86f57e27a43da
AndresTrujo <202360022@ucc.mx>
2025-06-03 12:16:38 -0600

    Cambios esteticos otra vez

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from .models import Order
 
+
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['full_name', 'email', 'address']
-        
+        widgets = {
+            'full_name': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 text-[#1E1E1E]'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 text-[#1E1E1E]'
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 text-[#1E1E1E]'
+            }),
+        }
